981fb9b8b3c7e337e1b82b228fe11d9994aa6980
Pietn <piet_buseyne@msn.com>
2015-10-02 21:48:45 +0200

    Alive or dead

--- gameoflife.js
+++ gameoflife.js
@@ -3,6 +3,11 @@
 	var context = canvas.getContext('2d');
 	var grid = createGrid();
 	
+	grid[1][1].alive = true;
+	grid[1][2].alive = true;
+	grid[1][3].alive = true;
+	
+	updateGrid();
 	drawGrid();
 	
 	function drawTile(x, y, color) {
@@ -18,11 +23,6 @@
 				grid[h][w] = { 
 					alive: false 
 				};
-				
-				/* For testing */
-				if (h === 0 || w === 0) {
-					grid[h][w].alive = true;
-				}
 			}
 		}
 		return grid;
@@ -37,4 +37,84 @@
 			}
 		}
 	}
-})(60, 45, document.getElementById('c'));
+	
+	function countNeighbors(x, y) {
+		var l = x - 1; /* left x */
+		if (l < 0) {
+			l = width - 1;
+		}
+		
+		var r = x + 1; /* right x */
+		if (r > width - 1) {
+			r = 0;
+		}
+		
+		var t = y - 1; /* top y */
+		if (t < 0) {
+			t = height - 1;
+		}
+		
+		var b = y + 1; /* bottom y */
+		if (b > height - 1) {
+			b = 0;
+		}
+		
+		var count = 0;
+		
+		if(grid[t][l].alive) {
+			count++;
+		}
+		if(grid[y][l].alive) {
+			count++;
+		}
+		if(grid[b][l].alive) {
+			count++;
+		}
+		
+		if(grid[t][x].alive) {
+			count++;
+		}
+		if(grid[b][x].alive) {
+			count++;
+		}
+		
+		if(grid[t][r].alive) {
+			count++;
+		}
+		if(grid[y][r].alive) {
+			count++;
+		}
+		if(grid[b][r].alive) {
+			count++;
+		}
+		
+		console.log(x, y, count);	
+		return count;
+	}
+	
+	function updateCell(x, y) {
+		var neighbors = countNeighbors(x, y);
+		if (grid[y][x].alive) {
+			return {
+				alive: neighbors === 2 || neighbors === 3
+			};
+		} else {
+			return {
+				alive: neighbors === 3
+			};
+		}
+	}
+	
+	function updateGrid() {
+		var newGrid = [];
+		
+		for (var h = 0; h < height; h++) {
+			newGrid[h] = [];
+			for (var w = 0; w < width; w++) {
+				newGrid[h][w] = updateCell(w, h);
+			}
+		}
+		
+		grid = newGrid;
+	}
+})(5, 5, document.getElementById('c'));
