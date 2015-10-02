a0e77d1195ddec255b28d4eae4c4aab79da8cbbe
Pietn <piet_buseyne@msn.com>
2015-10-02 20:57:33 +0200

    Creating and drawing grid

--- gameoflife.js
+++ gameoflife.js
@@ -1,11 +1,40 @@
 (function (width, height, canvas) {
 	var tileSize = 10;
 	var context = canvas.getContext('2d');
+	var grid = createGrid();
 	
-	drawTile(0, 0, 'blue');
-	drawTile(3, 10, 'red');
+	drawGrid();
+	
 	function drawTile(x, y, color) {
 		context.fillStyle = color;
 		context.fillRect (x * tileSize, y * tileSize, tileSize, tileSize);
+	}
+	
+	function createGrid() {
+		var grid = [];
+		for (var h = 0; h < height; h++) {
+			grid[h] = [];
+			for (var w = 0; w < width; w++) {
+				grid[h][w] = { 
+					alive: false 
+				};
+				
+				/* For testing */
+				if (h === 0 || w === 0) {
+					grid[h][w].alive = true;
+				}
+			}
+		}
+		return grid;
+	}
+	
+	function drawGrid() {
+		for (var h = 0; h < height; h++) {
+			for (var w = 0; w < width; w++) {
+				if (grid[h][w].alive) {
+					drawTile(w, h, 'black');
+				}
+			}
+		}
 	}
 })(60, 45, document.getElementById('c'));
